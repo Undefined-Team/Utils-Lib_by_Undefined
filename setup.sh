c94666c9956171b16cef244232cf1896c566b086
Timothée DAUTREME <tim.dautreme@hotmail.fr>
2019-09-30 12:20:07 +0200

    setup.sh 1.2

--- setup.sh
+++ setup.sh
@@ -155,7 +155,7 @@ else
         error_print "Copy headers files from $location/res/include/ to $ud_lib_path/include/ failed"
     fi
     # if !(make -C static $location LIBNAME="libud_$target_name.a" DEPNAME="$make_dep_name" > /dev/null 2>&1); then
-    if !(make -C static $location LIBNAME="libud_$target_name.a" DEPNAME="$make_dep_name"); then
+    if !(make -C $location static LIBNAME="libud_$target_name.a" DEPNAME="$make_dep_name"); then
         error_print "Compilation failed"
     fi
     if !(cp $location/*.a $ud_lib_path/lib/); then
