0974765b4b31d21499081a8f61db6055416f07e3
sbedene <sbedene@gmail.com>
2019-10-06 14:18:25 +0200

    test git pull error

--- setup.sh
+++ setup.sh
@@ -189,7 +189,7 @@ function start_recursive {
     ! $dep_recursive && { info_print "\n (3) Check if need update"; }
     if [[ "$noupdate" != "noupdate" ]] ; then
         (! gitret=$(git -C "$location" pull) && { error_print "Can't git pull" "\t"; }) > /dev/null 2>&1
-        info_print $gitret
+        info_print "$gitret"
         if [[ "$gitret" != "Already up to date." ]] ; then
             $dep_recursive && { info_print "[ $target_name ] need to be updated" "\t"; }
             success_print "Files updated" "\t"
