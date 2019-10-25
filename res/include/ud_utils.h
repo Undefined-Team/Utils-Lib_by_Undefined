52d1103f593449c0ef1386e2e0dd34f02353b8d3
Timothée DAUTREME <tim.dautreme@hotmail.fr>
2019-10-25 16:28:48 +0200

    fix

--- res/include/ud_utils.h
+++ res/include/ud_utils.h
@@ -52,7 +52,7 @@
         size_t len = sizeof(in_val) / sizeof(type); \
         new_arr = ud_ut_malloc(len * sizeof(type)); \
         type *p_new_arr = new_arr; \
-        for (ud_ut_count i = 0; i < len; ++i, ++p_new_arr, ++in_tmp) *p_new_arr = *in_tmp; new_arr; \
+        for (ud_ut_count i = 0; i < len; ++i) *p_new_arr++ = *in_tmp++; new_arr; \
     })
 
 # define ud_ut_sarray(type, ...) \
@@ -65,7 +65,7 @@
         { \
             new_arr = ud_ut_malloc(len * sizeof(type)); \
             type *p_new_arr = new_arr; \
-            for (ud_ut_count i = 0; i < len; ++i, ++p_new_arr, ++in_tmp) *p_new_arr = ud_str_dup(*in_tmp); \
+            for (ud_ut_count i = 0; i < len; ++i) *p_new_arr++ = ud_str_dup(*in_tmp++); \
         } \
         else new_arr = NULL; \
         new_arr; \
