6e2bf8fb57b0b20f00a33ae96201ff271d13408a
sbedene <sylvainbedene@gmail.com>
2019-10-27 15:12:22 +0100

    removed byte cmp

--- unit test/test.c
+++ unit test/test.c
@@ -1,6 +1,16 @@
 # include <ud_utils.h>
 # include <stdio.h>
 
+static int     ud_ut_byte_cmp_ctr(void *a, void *b, size_t n)
+{
+    char *p_a = (char *)a;
+    char *p_b = (char *)b;
+
+    for (ud_ut_count i = 0; i < n; ++i, ++p_a, ++p_b)
+        if (*p_a != *p_b) return (*p_a - *p_b);
+    return (0);
+}
+
 int main(void)
 {
     int a = 2;
